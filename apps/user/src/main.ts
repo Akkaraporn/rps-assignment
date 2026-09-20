295d16af79ba4425e87806b062b498c285814b48
Artty <akkaraporn1996@gmail.com>
2026-09-21 06:05:53 +0700

    scaffold user service

---
+++ apps/user/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(config.get<number>('USER_PORT') ?? 3002);
+}
+void bootstrap();
